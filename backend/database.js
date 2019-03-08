3329a64764ae32f56b1c19d93cdda08ab9c6a5ae
molatho <43727386+molatho@users.noreply.github.com>
2019-03-08 21:03:41 +0100

    Introducing tables and tokens to usermanagement

--- backend/database.js
+++ backend/database.js
@@ -11,6 +11,25 @@ class Database {
     static get() {
         return db;
     }
+
+    static checkTokens() {
+        var db = Database.get();
+        var tableNames = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett"];
+        var tables = db.get("tables").value();
+        var tokens = db.get("tokens").value();
+        if (!tables) { tables = {}; }
+        if (!tokens) { tokens = {}; }
+
+        for (var i = 0; i < tableNames.length; i++) {
+            if (tables[tableNames[i]] === undefined) {
+                tables[tableNames[i]] = {
+                    tokens: []
+                };
+            }
+        }
+    }
 }
+
+Database.initialize("data.json");
 
 module.exports = Database;
